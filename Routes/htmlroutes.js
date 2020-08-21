ce7f5df5f41c8d4dab78b224daa74f2b80c05915
Cassandra Perez <cassandraenriq@icloud.com>
2020-08-21 13:12:59 -0500

    updated file

--- Routes/htmlroutes.js
+++ Routes/htmlroutes.js
@@ -15,9 +15,9 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+  // // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
 
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
